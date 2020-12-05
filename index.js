5a873554b7e1bc4aba57ffc5e2fb48ff560ea1f9
mddalam1 <mddalam1@gmail.com>
2020-12-05 23:13:00 +0530

    Calling async method inside sync method and returning data.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+const getUser = id => {
+    setTimeout(() => { 
+        console.log("Reading a user from database...")
+        return { id : id , gitHubUsername : 'mdalam1' }
+    }, 2000)
+    // setTimeOut is an asyncronous method in js which doesn't block the execution.
+}
+
 console.log("Before")
-setTimeout(() => { console.log("Reading a user from database...")}, 2000)
-// setTimeOut is an asyncronous method in js which doesn't block the execution.
+const user = getUser(1)
+console.log(user)
 console.log("After")
+
