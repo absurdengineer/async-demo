78a7d0e3362e154d669d17fbea43d5a218ada4c6
mddalam1 <mddalam1@gmail.com>
2020-12-05 23:43:06 +0530

    Callbacks example

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-const getUser = id => {
+function getUser(id, callback){
     setTimeout(() => { 
-        console.log("Reading a user from database...")
-        return { id : id , gitHubUsername : 'mdalam1' }
+        console.log('Reading a user from database...')
+        callback({ id : id , gitHubUsername : 'mddalam1' })
     }, 2000)
     // setTimeOut is an asyncronous method in js which doesn't block the execution.
 }
+function getRepositories(username, callback){
+    setTimeout(() => {
+        console.log('Calling GitHub API... ')
+        if(username === 'mddalam1')
+            callback(['repo1', 'repo2', 'repo3'])
+        else
+            callback([])
+    }, 2000)
+}
 
-console.log("Before")
-const user = getUser(1)
-console.log(user)
+console.log('Before')   
+getUser(1, user => {
+    console.log('User : ', user)
+    getRepositories(user.gitHubUsername, repos => {
+        console.log("Repositories : " + repos)
+    })
+})
 console.log("After")
 
