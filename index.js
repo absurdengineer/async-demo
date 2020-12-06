92d2860206f858aa5dcc738db5ccd580262337ce
mddalam1 <mddalam1@gmail.com>
2020-12-06 16:18:49 +0530

    Callback Hell

--- index.js
+++ index.js
@@ -14,13 +14,47 @@ function getRepositories(username, callback){
             callback([])
     }, 2000)
 }
+function getRepoDetails(repo, callback){
+    setTimeout(() => {
+        console.log('Calling GitHub Repo API... ')
+        if(repo)
+            callback({name : "Express", commits : "7 commits"})
+        else
+            callback({})
+    }, 2000)
+}
 
 console.log('Before')   
 getUser(1, user => {
     console.log('User : ', user)
     getRepositories(user.gitHubUsername, repos => {
         console.log("Repositories : " + repos)
+        getRepoDetails(repos[0], repoDetail => {
+            console.log("Details of First Repo : ", repoDetail)
+        })
     })
 })
 console.log("After")
 
+/* 
+
+Synchronous Methods
+const user = getUser("mddalam1")
+const repos = getRepositories(user.username)
+const detail = getRepoDetails(repos[0])
+console.log(user,repos,detail)
+
+Asynchronous Methods
+getUser(1, user => {
+    console.log('User : ', user)
+    getRepositories(user.gitHubUsername, repos => {
+        console.log("Repositories : " + repos)
+        getRepoDetails(repos[0], repoDetail => {
+            // Again and Again calling Callbacks 
+            // This situation is called `Callback Hell`
+            console.log("Details : ", repoDetail)
+        })
+    })
+})
+
+*/
