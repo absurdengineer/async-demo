73363ea225c5ec8e15b58de9895c0932112e4dce
mddalam1 <mddalam1@gmail.com>
2020-12-08 11:49:42 +0530

    Solved Exercise with Promises( then and catch).

--- exercise.js
+++ exercise.js
@@ -1,34 +1,46 @@
-const getCustomer = (id, callback) => {
-    setTimeout(() => {
-        callback({
-            id : 1,
-            name : "Md Dilshad Alam",
-            isGold : true,
-            email : 'mdalam1@gmail.com'
-        })
-    }, 4000);
+const getCustomer = id => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve({
+                id : 1,
+                name : "Md Dilshad Alam",
+                isGold : true,
+                email : 'mdalam1@gmail.com'
+            })
+        }, 4000)
+    })
 }
-const getTopMovies = callback => {
-    setTimeout(() => {
-        callback(['movie1', 'movie2'])
-    }, 4000);
+const getTopMovies = () => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(['movie1', 'movie2'])
+        }, 4000)
+    })
 }
-const sendEmail = (email, movies, callback) => {
-    setTimeout(() => {
-        console.log("Sending ", movies, " to ", email, "...")
-        callback();
-    }, 4000);
+const sendEmail = (email, movies) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, 4000)
+    })
 }
 
-getCustomer(1, customer => {
-    console.log("customer : ", customer)
-    if(customer.isGold)
-        getTopMovies(movies => {
-            console.log(`Top Movies are : ${movies}`)
-            sendEmail(customer.email, movies, () => {
-                console.log("Email Sent.")
-            })
-        })
-    else
-        console.log("Not a Gold Customer")
-})
+getCustomer(1)
+    .then(customer => {
+        console.log("customer : ", customer)
+        if(customer.isGold)
+            getTopMovies()
+                .then(movies => {
+                    console.log(`Top Movies are : ${movies}`)
+                    console.log("Sending ", movies, " to ", customer.email, "...")
+                    sendEmail(customer.email, movies)
+                        .then(() => {
+                            console.log("Email Sent.")
+                        })
+                        .catch(err => console.log(`Error : ${err.message}`))
+                })
+                .catch(err => console.log(`Error : ${err.message}`))
+        else
+            console.log("Not a Gold Customer")
+    })
+    .catch(err => console.log(`Error : ${err.message}`))
